306195d9c7cdb75a149d4a2a6a4050a7e7a21c74
Alan Sproat <alan@sproatcentral.com>
2024-03-10 00:52:57 -0600

    Almost done with web ratings.

--- src/components/DinderForm/DinderForm.tsx
+++ src/components/DinderForm/DinderForm.tsx
@@ -1,6 +1,6 @@
-import { memo } from 'react';
+import { Component, memo } from 'react';
 import type { FC } from 'react';
-import ReactPlayer from 'react-player'
+import ReactDomServer from 'react-dom/server';
 import resets from '../_resets.module.css';
 import { BrandsFacebook } from '../Website/BrandsFacebook/BrandsFacebook.js';
 import { BrandsInstagram } from '../Website/BrandsInstagram/BrandsInstagram.js';
@@ -16,6 +16,8 @@ import { TextArea_labelTrue } from '../Website/TextArea_labelTrue/TextArea_label
 
 import { Amplify, API } from 'aws-amplify';
 import { basename } from 'path';
+import { int } from 'aws-sdk/clients/datapipeline';
+import React from 'react';
 
 interface Props {
   className?: string;
@@ -26,7 +28,6 @@ interface Props {
   dinderinvitecode: string;
   setDinderInviteCode(d: string): void;
 }
-
 
 export const DinderForm: FC<Props> = memo(function DinderForm(this: any, { page, setPage, dinder, setDinder, dinderinvitecode, setDinderInviteCode }) {
 
@@ -59,7 +60,7 @@ export const DinderForm: FC<Props> = memo(function DinderForm(this: any, { page,
   console.log("show invite")
   console.log(dinderinvitecode)
   console.log("end show")
-  document.getElementById("invitation")!.style.display = "flex"
+  //document.getElementById("invitation")!.style.display = "flex"
 
   if(lastPart == "dinderform") {
     setPage("dinderform")
@@ -85,6 +86,7 @@ export const DinderForm: FC<Props> = memo(function DinderForm(this: any, { page,
 
   var placeList: string[] = []
   var placeDetails = JSON.parse("{\"a\":\"b\"}")
+  var placeValues = JSON.parse("{\"a\":\"b\"}") 
   var choices = ""
   Object.keys(dinder.choices).toSorted().map((key, index) => (
     placeList.push(key.substring(4))
@@ -103,7 +105,72 @@ export const DinderForm: FC<Props> = memo(function DinderForm(this: any, { page,
     }
   };
 
-  function getPlaces() {
+function ratingClick(placeId: string, rating: int) {
+  if(placeValues[placeId] == rating) {
+    placeValues[placeId] = 0
+    if(dinder.dinderOptions.maxDoublePlus > 0) {
+      document.getElementById(placeId + "2up")!.className = classes.choiceButton2UpOutline
+    }
+    document.getElementById(placeId + "1up")!.className = classes.choiceButton1UpOutline
+    document.getElementById(placeId + "1down")!.className = classes.choiceButton1DownOutline
+    if(dinder.dinderOptions.maxDealBreaker > 0) {
+      document.getElementById(placeId + "2down")!.className = classes.choiceButton2DownOutline    
+    }
+  } else {
+    placeValues[placeId] = rating
+    if(dinder.dinderOptions.maxDoublePlus > 0) {
+      document.getElementById(placeId + "2up")!.className = classes.choiceButton2UpOutlineFaded
+    }
+    document.getElementById(placeId + "1up")!.className = classes.choiceButton1UpOutlineFaded
+    document.getElementById(placeId + "1down")!.className = classes.choiceButton1DownOutlineFaded
+    if(dinder.dinderOptions.maxDealBreaker > 0) {
+      document.getElementById(placeId + "2down")!.className = classes.choiceButton2DownOutlineFaded    
+    }
+    switch(rating) {
+      case 2:
+        document.getElementById(placeId + "2up")!.className = classes.choiceButton2UpSolid;
+        break;
+      case 1:;
+        document.getElementById(placeId + "1up")!.className = classes.choiceButton1UpSolid;
+        break;
+      case -1:
+        document.getElementById(placeId + "1down")!.className = classes.choiceButton1DownSolid;
+        break;
+      case -2:
+        document.getElementById(placeId + "2down")!.className = classes.choiceButton2DownSolid;
+      }
+  }
+}
+
+function ratingClick2(placeId: string, rating: int) {
+  console.log(placeId + ":" + rating);
+}
+
+
+function ratingDiv(placeId: string, rating: int) {
+    return <div id={placeId + Math.abs(rating) + (rating > 0 ? "up" : "down") }
+        className={
+          (rating == 2 ? classes.choiceButton2UpOutline :
+            (rating == 1 ? classes.choiceButton1UpOutline :
+              (rating == -1 ? classes.choiceButton1DownOutline :
+                classes.choiceButton2DownOutline
+              )
+            )
+          )
+        }
+      ></div>
+}
+
+function setRatingClick(placeId: string, rating: int) {
+   var ratingDiv = document.getElementById(placeId + Math.abs(rating) + 
+      (rating > 0 ? "up" : "down")
+    )
+    ratingDiv?.addEventListener("click", (event) => {
+      ratingClick(placeId, rating);
+    });
+}
+
+function getPlaces() {
   API.post(apiName, path, myInit)
   .then((response) => {
     console.log("responseDF")
@@ -111,47 +178,22 @@ export const DinderForm: FC<Props> = memo(function DinderForm(this: any, { page,
     if (response.data != null) {
       console.log(response.data)
       placeDetails=JSON.parse(response.data.body)
-      var choicesTable = document.getElementById("choices")! as HTMLTableElement
-      if(choicesTable.rows.length == 0) {
-      var choicesHeader = choicesTable.createTHead()
-      var choicesHeaderRow = choicesHeader.insertRow()
-      choicesHeaderRow.insertCell().outerHTML = "<th>Choice Info</th>"
-      var ratingsTitle = choicesHeaderRow.insertCell()
-      ratingsTitle.outerHTML = "<th>Rating</th>"
-      ratingsTitle.colSpan = 2 
-      var choicesBody = choicesTable.createTBody()
       for(const placeId of placeList ) {
-        var choiceRow = choicesBody.insertRow()
-        var thisPlace = placeDetails[String(placeId)]
-        var detailsCell = choiceRow.insertCell()
-        detailsCell.className = classes["choiceDetails"]
-        detailsCell.innerHTML = thisPlace["name"] + "<br/>" +
-          thisPlace["formatted_address"] + "<br/>" +
-          "Rating: " + "*".repeat(thisPlace["rating"]) + " " + String(thisPlace["rating"]) + 
-          " (" + String(thisPlace["user_ratings_total"]) + ") <br/>" +
-          (!("price_level" in thisPlace) ? "" : ("Price Level: " + "$".repeat(thisPlace["price_level"]) + "<br/>")) + 
-          "<a href=" + "https://www.google.com/maps/search/?api=1&query=Google&query_place_id=" + 
-          placeId + " target='_blank'>View Details</a>"
-        if(dinder.dinderOptions.maxDoublePlus > 0) {
-          var doubleupcell = choiceRow.insertCell()
-          doubleupcell.className = classes["choiceCell"]
-          doubleupcell.innerHTML = "<div id='two_up' class='" + classes["choiceButton2UpOutline"] + "'></div>"
-        }
-        var upcell = choiceRow.insertCell()
-        upcell.className = classes["choiceCell"]
-        upcell.innerHTML = "<div id='one_up' class='" + classes["choiceButton1UpOutline"] + "'></div>"
-        var downcell = choiceRow.insertCell()
-        downcell.className = classes["choiceCell"]
-        downcell.innerHTML = "<div id='one_down' class='" + classes["choiceButton1DownOutline"] + "'></div>"
-        if(dinder.dinderOptions.maxDealBreaker > 0) {
-          var doubledowncell = choiceRow.insertCell()
-          doubledowncell.className = classes["choiceCell"]
-          doubledowncell.innerHTML = "<div id='two_down' class='" + classes["choiceButton2DownOutline"] + "'></div>"
-        }
+        placeValues[placeId] = 0
       }
-      document.getElementById("choices")!.style.display="block"
       document.getElementById("choiceswait")!.style.display = "none"
-    }
+      document.getElementById("choices")!.outerHTML = ReactDomServer.renderToString(getDetailTable())
+      document.getElementById("choices")!.style.display="block"
+      for(const placeId of placeList ) {
+        if(dinder.dinderOptions.maxDoublePlus > 0) {
+          setRatingClick(placeId, 2)
+        }
+        setRatingClick(placeId, 1)
+        setRatingClick(placeId, -1)
+        if(dinder.dinderOptions.maxDealBreaker > 0) {
+          setRatingClick(placeId, -2)
+        }
+      }      
     }
   })
   .catch((error) => {
@@ -164,11 +206,47 @@ export const DinderForm: FC<Props> = memo(function DinderForm(this: any, { page,
     }
   }
   );
-}
+};
+
+  function getDetailTable() : React.JSX.Element {
+    var placeRows = placeList
+
+    const tableElement =  <table id="choices" style={{ display: 'none' }}>
+      <thead>
+        <th>Choice Info</th>
+        <th colSpan={4}>Ratings</th>
+      </thead>
+      {placeRows.map(key =>
+        <tr key={key + "row"}>
+          <td className={classes.choiceDetails}>
+            {placeDetails[key].name}<br/>
+            {placeDetails[key].formatted_address}<br/>
+            Price Level: {"$".repeat(placeDetails[key].price_level)}<br/>
+            <div className={classes.choiceRatingDetails}>Rating:<span className={classes.bigStar}> {"*".repeat(placeDetails[key].rating)} </span>{placeDetails[key].rating} ({placeDetails[key].user_ratings_total})</div>
+            <a href='https://www.google.com/maps/search/?api=1&query=Google&query_place_id={key}' target='_blank'>View Details</a>
+          </td>
+          <td className={classes.choiceCell}>
+            { ratingDiv(key, 2) } 
+          </td>
+          <td className={classes.choiceCell}>
+            { ratingDiv(key, 1) }
+          </td>
+          <td className={classes.choiceCell}>
+            { ratingDiv(key, -1) } 
+          </td>
+          <td className={classes.choiceCell}>
+            { ratingDiv(key, -2) } 
+          </td>
+        </tr>
+      )}
+    </table>
+
+    return tableElement
+  }
 
   console.log("return dinder?")
   console.log(dinder)
-
+  
   /* @figmaId 154:24 */
   return (
     <div className={`${resets.clapyResets} ${classes.root}`}>
@@ -196,23 +274,25 @@ export const DinderForm: FC<Props> = memo(function DinderForm(this: any, { page,
 
       </div>
       <div className={classes.nosplit}>
+        <script>
+        </script>
         <div className={classes.frame2760}>
-          <div className={classes.welcomeNameOfUser}>The Dinder App: </div>
+          <div className={classes.headline1}>The Dinder App: </div>
           <div className={classes.headline2}>Finally, the answer to "Where are we going out to eat?"</div>
             <div className={classes.welcomeNameOfUser} id="welcome">
               Okay { dinder.privateIds[dinderinvitecode].sentName },
             </div>
-            <div className={classes.textBlock3} style={{display:"block"}}>
+            <div className={classes.textBlock3} style={{display:"block", marginBlockEnd: 0 }}>
               <p>You can use this webpage to rate the restaurants for {dinder.dindername}, but... </p>
               <ul>
                 <li>you have to rate them all now, you can't come back and finish</li>
                 <li>you also can't change them later</li>
                 <li>you won't be able to see what ratings others have given</li>
               </ul>
-              <p>If you've changed your mind, you can download the app below and click the link you were sent again.</p>
-              <p className={ classes.noThanksIMNotDownloadingYourApp } onClick={() => { console.debug("getPlaces"); document.getElementById("choiceswait")!.style.display = "block"; getPlaces(); }}>Otherwise, click here to see the choices.</p>
+              <p style={{marginBottom: 8 }}>If you've changed your mind, you can download the app below and click the link you were sent again.</p>
+              <p className={classes.noThanksIMNotDownloadingYourApp} onClick={() => { console.debug("getPlaces"); document.getElementById("choiceswait")!.style.display = "block"; getPlaces(); }}>Otherwise, click here to see the choices.</p>
             </div>
-            <div className={classes.downloadButtons}>
+            <div className={classes.downloadButtons} >
             <div>
               <a href="itms-services://?action=download-manifest&amp;url=https://www.thedinderapp.com/app/manifest.plist" >
                 <img className={classes.applebadge} src="https://tools.applemediaservices.com/api/badges/download-on-the-app-store/black/en-us?size=250x83&amp;releaseDate=1531008000" alt="Download on the App Store" ></img>
@@ -226,8 +306,8 @@ export const DinderForm: FC<Props> = memo(function DinderForm(this: any, { page,
             <div className={classes.textBlock3}>Loading choices...</div>
             <progress id="progress" style={{ all: "revert" }} max="100" />
           </div>
-          <table id="choices" style={{ display: "none" }} >
-          </table>
+          <div id="choices" style={{ display: "none" }} >
+          </div>
         </div>
       </div>
       <div className={classes.appDownload}>
